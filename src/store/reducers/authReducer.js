c17eb2084897e2b2ac8a087117f07b5313419fff
Student <example@example.com>
2019-09-25 15:35:24 -0700

    left off part 24

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,9 +1,23 @@
 
 const initState = {
-
+    authError: null
 }
 
 const authReducer = (state = initState, action) => {
-    return state;
+
+    switch (action.type) {
+        case 'LOGIN_ERROR':
+            console.log('errorrr');
+            return {
+                ...state,
+                authError: ' login failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('success');
+            return {
+                ...state, authError: null
+            }
+        default: return state;
+    }
 }
 export default authReducer;
